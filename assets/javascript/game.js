87e164f4bd67cc0d0e67a1eeaf9dd97bfd34e1a2
1curtislee <1curtislee@gmail.com>
2019-02-08 14:20:51 -0800

    register keypress and check if included in secret word

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,43 +1,21 @@
 var secretWord = "backscratcher";
+console.log("Secret Word: ", secretWord);
 
-function registerKeypress() {
-  console.log(event.key);
-
-  if (event.key != secretWord.substring()) {
-    console.log("not in word");
-  } 
-  /* 
-  if (event.key != secretWord.substring(1, 2)) {
-    console.log("not in word");
-  }
-  if (event.key != secretWord.substring(2, 3)) {
-    console.log("not in word");
-  }
-  if (event.key != secretWord.substring(3, 4)) {
-    console.log("not in word");
-  }
-  if (event.key != secretWord.substring(4, 5)) {
-    console.log("not in word");
-  }
-  if (event.key != secretWord.substring(5, 6)) {
-    console.log("not in word");
-  }
-  if (event.key != secretWord.substring(6, 7)) {
-    console.log("not in word");
-  }
-  if (event.key != secretWord.substring(7, 8)) {
-    console.log("not in word");
-  } 
-  */
-
-  if (event.key == secretWord.substring(0, 1)) {
-    console.log("YES!")
-  }
+function registerKeyPress() {
+  var keyPressed = event.key;
+  console.log("key pressed", keyPressed)
+  var incl = secretWord.includes(keyPressed);
+  console.log("incl", incl);
 }
 
-function remainingGuessesDecrease() {
 
-}
+
+
+
+
+
+
+
 
 
 
