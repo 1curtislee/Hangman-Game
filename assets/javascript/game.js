5893c0e1685bf2bb2923986fa835af42bb58179a
1curtislee <1curtislee@gmail.com>
2019-02-09 13:57:24 -0800

    more work

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,23 +1,41 @@
-function hangmanGameStart() {
-  
-  
+function gameStart() {
+
+}
+
+
+
+
+var remainingGuesses = 20;
+
+function decreaseGuesses() {
+  remainingGuesses--;
 }
 
 function checkLetter() {
   var secretWord = "backscratcher";
-  var remainingGuesses = 20;
   var keyPressed = event.key;
+  console.log(event.key);
   var incl = secretWord.includes(keyPressed);
   var guesses = document.getElementById("guessCount");
-  
+  var letters = document.getElementById("wrongLetters");
   if (incl == true) {
-    console.log("it's in there");
+    console.log("GOT ONE!");
   } 
   if (incl == false) {
     console.log("nope");
   }
-  console.log(remainingGuesses)
+  
+  var guessedArray = [0, 1, 2, 3]
+  guessedArray[2] = event.key;
+
+    //Populating body:
+  secretWordLetters.innerText = "* * * * * * * * * * * * *"
+  letters.innerText = guessedArray;
   guesses.innerText = remainingGuesses;
+
+  if (remainingGuesses == 0) {
+    alert("GAME OVER");
+  }
 }
 
 
